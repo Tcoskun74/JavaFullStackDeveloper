249b8676639998e00da40ad39032ddfa50ad35c3
tcoskun74 <tunahancoskun74@gmail.com>
2022-07-22 22:02:28 +0300

    Initial commit

---
+++ src/main/java/com/tunahancoskun/TunahanCoskunApplication.java
@@ -0,0 +1,13 @@
+package com.tunahancoskun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunahanCoskunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunahanCoskunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tunahancoskun/TunahanCoskunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunahancoskun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunahanCoskunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
